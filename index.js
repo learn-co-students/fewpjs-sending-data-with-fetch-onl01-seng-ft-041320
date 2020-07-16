00a8d29b673a0d2870450bed30df938b834d3f02
rschlosser11 <rosalinschlosser@gmail.com>
2020-07-16 15:17:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch ("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    }).then(function(response) {
+        return response.json();
+    }).then(function(object) {
+        console.log(object);
+        let p = document.createElement('p');
+        p.textContent = object.id;
+        document.body.appendChild(p);
+    }).catch(function(error) {
+        console.log(error.message);
+        let p = document.createElement('p');
+        p.textContent = error.message;
+        document.body.appendChild(p);
+    });
+}
+
