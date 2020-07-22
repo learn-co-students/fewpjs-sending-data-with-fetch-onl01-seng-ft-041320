b945edaf6e93da2b43fe66247fa4f30926db99c2
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-07-21 22:27:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+
+// Test 1
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name,
+                email
+            })
+        })
+
+// Test 2
+
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (object) {
+            document.body.innerHTML = object.id
+        })
+
+// Test 3
+
+        .catch(function (error) {
+            document.body.innerHTML = error.message
+        });
+
+
+
+}
