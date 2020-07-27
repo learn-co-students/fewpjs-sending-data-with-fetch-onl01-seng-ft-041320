7ebbecb600a435e4744753b001dbbe010bf8b789
codeweb123 <jiyonj@hotmail.com>
2020-07-27 18:11:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+const submitData = (name, email) => {
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email
+      })
+    })
+    .then(resp => {
+      return resp.json()
+    })
+    .then(data => {
+      document.body.innerHTML += data.id
+    })
+    .catch(err =>{
+      document.body.innerHTML += err.message
+    })
+  }
