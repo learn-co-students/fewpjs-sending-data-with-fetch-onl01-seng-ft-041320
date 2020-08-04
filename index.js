36e6e8b8009f14e83c42a513083720f3db501ff0
Franklyn Rodriguez <franklynr92@gmail.com>
2020-08-04 12:51:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+let userData = {
+    name: "John",
+    email: "john@gmail.com"
+};
+let postData = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+};
+function submitData(){
+  fetch("http://localhost:3000/users", postData);
+};
+//Challlenges/test
+//In submitData, write a valid POST request to http://localhost:3000/users using fetch(). 
+//This request should include: The destination URL
+//Headers for 'Content-Type' and 'Accept' set to 'application/json'[]
+//A body with the name and email passed in as arguments to `submitData` as keys in an obj [] 
+//This object should then be stringified. []
+//
